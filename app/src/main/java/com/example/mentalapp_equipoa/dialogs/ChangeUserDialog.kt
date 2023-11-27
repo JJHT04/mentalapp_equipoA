c410c03506c1918efc404658ab651b63fb9fbd32
Jorge <portidelga@gmail.com>
2023-11-27 19:24:42 +0100

    Guapos, nuevo menu

--- app/src/main/java/com/example/mentalapp_equipoa/dialogs/ChangeUserDialog.kt
+++ app/src/main/java/com/example/mentalapp_equipoa/dialogs/ChangeUserDialog.kt
@@ -27,7 +27,7 @@ class ChangeUserDialog: DialogFragment() {
                 }
                 .setPositiveButton("Accept") {
                     _,_ ->
-                    val text = requireDialog().findViewById<TextView>(R.id.email).text.toString()
+                    val text = requireDialog().findViewById<TextView>(R.id.Age).text.toString()
                     if (text.isNotBlank()) {
                         userName = text
                         Toast.makeText(activity, "User name changed successfully", Toast.LENGTH_SHORT).show()
