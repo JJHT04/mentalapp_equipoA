5c1e0c4dafcf9234f4f1be94cc9bd56a54847457
José Javier <127211374+JJHT04@users.noreply.github.com>
2023-11-27 20:15:14 +0100

    Rama javier (#5)
    * mejorado un poquito la interfaz de test :3
    
    * mejorado un poquito la interfaz de test :3
    
    * se ha hecho la interfaz responsive, ya que en tablets no se veía bien
    
    * Cambiado cositas de la toolbar y Se ha creado la actividad UserGuideActivity
    
    * Cambiado cositas de la toolbar y Se ha creado la actividad UserGuideActivity

--- app/src/main/java/com/example/mentalapp_equipoa/TestActivity.kt
+++ app/src/main/java/com/example/mentalapp_equipoa/TestActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 
