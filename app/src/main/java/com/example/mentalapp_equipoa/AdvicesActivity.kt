2710f19067c54586941fbc23bc5649dcd08ed6b2
Javier <javier_5_2010@hotmail.com>
2023-12-11 23:23:14 +0100

    cositas con el icono

---
+++ app/src/main/java/com/example/mentalapp_equipoa/AdvicesActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mentalapp_equipoa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AdvicesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_advices)
+    }
+}
